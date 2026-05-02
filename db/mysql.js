2563e7413c141c7d68456895e742310c39168355
EternalProg <eternal.proglife@gmail.com>
2026-05-02 19:41:07 +0300

    task 1: add mysql pool plugin with startup and close hooks

---
+++ db/mysql.js
@@ -0,0 +1,32 @@
+import fp from 'fastify-plugin';
+import mysql from 'mysql2/promise';
+
+async function mysqlPlugin(fastify) {
+  const pool = mysql.createPool({
+    host: fastify.config.MYSQL_HOST,
+    port: fastify.config.MYSQL_PORT,
+    user: fastify.config.MYSQL_USER,
+    password: fastify.config.MYSQL_PASSWORD,
+    database: fastify.config.MYSQL_DB,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+
+  try {
+    await pool.query('SELECT 1');
+  } catch (error) {
+    await pool.end();
+    fastify.log.error({ err: error }, 'MySQL connection failed');
+    throw error;
+  }
+
+  fastify.decorate('mysql', pool);
+  fastify.decorate('db', pool);
+
+  fastify.addHook('onClose', async () => {
+    await pool.end();
+  });
+}
+
+export default fp(mysqlPlugin, { name: 'mysql' });
